94cc5082c20755d122a894ad98b1e8c7a9234deb
Jason Ho <git@hogt.me>
2024-10-24 18:47:24 -0700

    refactor database.js, add more flexible qSelect* query builders

--- database.js
+++ database.js
@@ -1,6 +1,10 @@
 const mysql = require('mysql');
 const { getCircularReplacer } = require('./util');
 
+/**
+ * Build a MySQL connection based on the environment variables specified with DB_*
+ * @returns {mysql.Connection} A MySQL connection object.
+ */
 function getConnection() {
     return mysql.createConnection({
         host: process.env['DB_HOST'],
@@ -10,6 +14,13 @@ function getConnection() {
     });
 }
 
+/**
+ * Executes a query statement with the specified bindings.
+ *   A query can be a Prepared Statement, and bindings values can be specified in the `bindings` argument.
+ * @param {String} query The SQL query.
+ * @param {Array<*>} bindings Bindings for the Prepared Statement (if any), otherwise an empty array should be used.
+ * @returns {Object} An object representing the results where keys = columns and values = data.
+ */
 async function query(query, bindings = []) {
     console.log(`Query: ${query} | Bindings: ${JSON.stringify(bindings, getCircularReplacer())}`);
     return await new Promise((resolve, reject) => {
@@ -28,26 +39,104 @@ async function query(query, bindings = []) {
     });
 }
 
+/**
+ * Generates a SELECT query.
+ * @param {String} table 
+ * @param {String} rawConditions 
+ * @param {String} rawColumns 
+ * @returns {String}
+ */
+function qSelect(table, rawConditions = '', rawColumns = '*') {
+    rawConditions = rawColumns.length > 0 ? (' WHERE ' + rawConditions) : rawConditions;
+    return `SELECT ${rawColumns} FROM ${table}${rawConditions}`;
+}
+
+/**
+ * Generates a query for counting the number of rows in a table (optionally) given conditions.
+ * @param {String} table 
+ * @param {String} rawConditions 
+ * @returns {String}
+ */
+function qCount(table, rawConditions = null) {
+    // format the query properly if conditions are specified
+    return qSelect(table, rawConditions, 'count(*)');
+}
+
+/**
+ * Generates a query for selecting rows based on `repeat` filters and a single repeating clause.
+ * @param {String} table 
+ * @param {String[]} columns 
+ * @param {String} join_phrase 
+ * @param {String} rawColumns 
+ * @returns {String}
+ * @throws {Error}
+ */
+function qSelectID_Plural(table, columns, join_phrase = 'OR', rawColumns = '*') {
+    if (columns.length < 1) throw new Error(`Invalid column count: ${columns}`);
+    let where_conditions = columns.map(col => `\`${col}\` = ?`).join(` ${join_phrase} `);
+    return qSelect(table, where_conditions, rawColumns);
+}
+
+/**
+ * Genereates a query to lookup rows by one specific identifying column.
+ * @param {String} table 
+ * @param {String} idCol 
+ * @returns {String}
+ */
 function qSelectID(table, idCol = 'id') {
-    return "SELECT * FROM `" + table + "` WHERE `" + idCol + "` = ?;";
+    return qSelectID_Plural(table, [idCol], 'AND');
 }
 
+/**
+ * Generates a query for inserting a row into `table` using `requiredCols` columns.
+ * @param {String} table 
+ * @param {String[]} requiredCols 
+ * @returns {String}
+ */
 function qInsertion(table, requiredCols) {
-    var build =  "INSERT INTO `" + table + "` ";
-    build += "(";
-    var first = true;
-    for (let key of requiredCols) {
-        build += (first ? "" : ", ") + "`" + key + "`";
-        first = false;
-    }
-    build += ") VALUES (";
-    var first = true;
-    for (let key of requiredCols) {
-        build += (first ? "" : ", ") + "?";
-        first = false;
-    }
-    build += ");";
-    return build;
+    let columns = requiredCols.map(column => `\`${column}\``).join(', ');
+    let values = requiredCols.map(_ => '?').join(', ');
+
+    return `INSERT INTO \`${table}\` (${columns}) VALUES (${values});`;
 }
 
-module.exports = {getConnection, query, qSelectID, qInsertion};
+const ALIAS_RESULT_COLUMN = 'FINAL';
+/**
+ * Generates a statement for aliasing different table's values into appropriately named columns
+ *   to be used in SELECT queries involving JOIN statements.
+ * @param {String[]} tables The JOINed tables.
+ * @returns {String} (table of tables), (column of table) => `table`.`column` as 'table.column', {...}
+ */
+async function getColumnsAsSelectAliases(tables) {
+    console.log("tables: ", tables);
+
+    let prepared_fields = new Array(tables.length).fill('?').join(', ');
+    let q = qSelect(
+        '`information_schema`.`columns`', 
+        ['table_schema = ?', `table_name IN(${prepared_fields})`].join(' AND '),
+        `group_concat(concat('\`', table_name, '\`.\`', column_name, '\` as \\'', table_name, '.', column_name, '\\'') SEPARATOR ', ') as ${ALIAS_RESULT_COLUMN}`)
+    /* Equivalent to:
+        `SELECT
+            group_concat(concat('\`', table_name, '\`.\`', column_name, '\` as \\'', table_name, '.', column_name, '\\'') SEPARATOR ', ') as ${ALIAS_RESULT_COLUMN}
+        FROM
+            \`information_schema\`.\`columns\`
+        WHERE
+            table_schema = ?
+            AND table_name IN(
+                ${prepared_fields}
+        );`
+    */
+
+    console.log("query: ", q);
+
+    let database = getConnection().config.database;
+    let results = await query(q, [database, ...tables]);
+    if (results.length <= 0) {
+        throw new Error(`Cannot build columns as select aliases for tables: '${tables}'`);
+    }
+    let aliases = results[0][ALIAS_RESULT_COLUMN];
+    console.log('built aliases: ', aliases);
+    return aliases;
+}
+
+module.exports = {getConnection, query, qCount, qSelectID_Plural, qSelectID, qInsertion, getColumnsAsSelectAliases};
